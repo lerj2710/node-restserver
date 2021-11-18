90935abf8a68dcdc6b2a1d50305d4dde251b7985
luis rodriguez <kike.lerj2710@gmail.com>
2021-11-18 13:48:22 -0500

    configurarndo el puerto de env

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hola mundo')
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT
+app.get('/', (req, res)=>{
+    res.send('hola mundo desde node y express')
+})
+.listen(port, ()=>{
+    console.log(`corriendo en puerto ${port}`)
+})
