90702baa3db98a3269dca0e6ab87a775932d212a
luis rodriguez <kike.lerj2710@gmail.com>
2021-11-18 14:25:59 -0500

    se agrego middleware y carpta publica

--- models/server.js
+++ models/server.js
@@ -6,10 +6,18 @@ class Server{
     constructor(){
        this.app = express();
        this.port = process.env.PORT
+
+       //middleware || vista de mi app
+       this.middleware();
+       //rutas de mi app
        this.routers();
     }
+
+    middleware(){
+        this.app.use( express.static('public'))
+    }
     routers(){
-        this.app.get('/', (req, res)=>{
+        this.app.get('/api', (req, res)=>{
             res.send('hola mundo desde node y express')
         })
     }
