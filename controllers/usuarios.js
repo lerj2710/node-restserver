02d9bb8bd3824d4621ffc56f00af26310ac7d331
luis rodriguez <kike.lerj2710@gmail.com>
2021-11-19 17:51:04 -0500

    encriptando contraseña con bcryptjs y quitar validacion de google

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,6 @@
 const {response, request}= require('express');
-const Usuario = require('../models/usuario')
+const Usuario = require('../models/usuario');
+const bcryptjs = require('bcryptjs')
 
 const usuariosGet =  (req= request, res = response)=>{
    
@@ -13,9 +14,19 @@ const usuariosGet =  (req= request, res = response)=>{
     })
 }
 const usuariosPost= async(req, res = response)=>{
-    const body = req.body
-    const usuario = new Usuario(body);
+    
+    const {nombre, correo, password, rol} = req.body
+    const usuario = new Usuario({nombre, correo, password, rol});
+    //autenticar correo
+
+    //encriptar la contraseña
+
+    const salt = bcryptjs.genSaltSync();
+          usuario.password = bcryptjs.hashSync(password, salt);
+
+    //guardar en base de datos
     await usuario.save();
+    
     res.json({
         
         usuario
