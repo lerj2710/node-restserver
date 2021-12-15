5fb71a30c2725082c78a89eecee7fedb3dca5233
luis rodriguez <kike.lerj2710@gmail.com>
2021-12-15 10:23:46 -0500

    se valido la extensines img

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -12,16 +12,27 @@ const cargarArchivo = async (req, res = response)=>{
   }
 
   const { archivo } = req.files;
+  const nombreCortado = archivo.name.split('.');
+  
+  const extension = nombreCortado[ nombreCortado.length - 1 ];
+  // validar la extension
+  const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
 
-const  uploadPath  = path.join( __dirname , '../uploads/' , archivo.name);
-
-  archivo.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({ err });
+    if(!extensionesValidas.includes(extension)){
+      return res.status(400).json({
+        msg: `la extensiom ${ extension } no es valida. - extensiones validas son ${ extensionesValidas}` 
+      })
     }
+  res.json({ extension })
+// const  uploadPath  = path.join( __dirname , '../uploads/' , archivo.name);
 
-    res.json({ msg: 'File uploaded to ' + uploadPath });
-  });
+//   archivo.mv(uploadPath, (err) => {
+//     if (err) {
+//       return res.status(500).json({ err });
+//     }
+
+//     res.json({ msg: 'File uploaded to ' + uploadPath });
+//   });
 
 };
 
