9e87f1f09b6226ba8a1526125660ea3faaca8c95
luis rodriguez <kike.lerj2710@gmail.com>
2021-12-15 10:03:01 -0500

    se regresando un paso atras y corrigiendo algunos errores

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const { response } = require("express");
 const { subirArchivo } = require('../helpers/subir-archivo');
 
@@ -5,16 +6,22 @@ const { subirArchivo } = require('../helpers/subir-archivo');
 const cargarArchivo = async (req, res = response)=>{
 
 
-  //   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-  //     res.status(400).json({msg: 'no hay archivos en la peticion'});
-  //     return;
-  //   }
-  // const nombre = await subirArchivo(req.files);
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo ) {
+    res.status(400).json({ msg: 'no hay archivos que subir.'});
+    return;
+  }
 
-  res.json({
-      // nombre
-      msg: 'hola mundo'
-  })
+  const { archivo } = req.files;
+
+const  uploadPath  = path.join( __dirname , '../uploads/' , archivo.name);
+
+  archivo.mv(uploadPath, (err) => {
+    if (err) {
+      return res.status(500).json({ err });
+    }
+
+    res.json({ msg: 'File uploaded to ' + uploadPath });
+  });
 
 };
 
